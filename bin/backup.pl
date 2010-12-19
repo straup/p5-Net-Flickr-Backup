7ef5341ee7612304175598b3193d25b451f34488
straup <github@aaronland.net>
2010-12-19 11:33:44 -0800

    example backup script

---
+++ bin/backup.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+# -*-perl-*-
+
+use strict;
+
+use Net::Flickr::Backup;
+use Log::Dispatch::Screen;
+use Getopt::Std;
+
+{
+  &main();
+  exit;
+}
+
+sub main {
+
+  my %opts = ();
+  getopts('c:v', \%opts);
+
+  my $cfg = Config::Simple->new($opts{'c'}) || die $!;
+
+  my $flickr = Net::Flickr::Backup->new($cfg);
+    
+  if ($opts{'v'}){
+      my $debug = Log::Dispatch::Screen->new('name' => 'debug', 'min_level' => 'debug');
+      $flickr->log()->add($debug);
+  }
+
+  $flickr->backup(); 
+  return 1;
+}
+
